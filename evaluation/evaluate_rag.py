6eb1d9ae9bf814b62abc35b136d640b34426f3f5
NhatAnh1801 <vuongnhatanh10@gmail.com>
2026-03-31 18:18:12 +0700

    commit test_data.json

--- evaluation/evaluate_rag.py
+++ evaluation/evaluate_rag.py
@@ -1,8 +1,16 @@
 from langchain.chat_models import init_chat_model
 from ragas import EvaluationDataset
+from ragas import evaluate
+from ragas.llms import LangchainLLMWrapper
+from ragas.metrics import LLMContextRecall, Faithfulness, FactualCorrectness
 
 from dotenv import load_dotenv
 from src.rag_engine import RagController
+import json
+
+# Load test_data.json as a ragas EvaluationDataset
+test_path = "evaluation/dataset/test_data.json"
+test_data = EvaluationDataset.from_jsonl(test_path)
 
 import os
 
@@ -14,14 +22,15 @@ class RagEvaluation:
         if not GEMINI_API_KEY:
             raise ValueError("GEMINI_API_KEY environment variable is not set")
         
-        llm_model = "google_genai:gemini-flash-lite-latest"
+        model = "google_genai:gemini-flash-lite-latest"
         
-        self.llm_judge = init_chat_model(
-            model = llm_model,
+        llm = init_chat_model(
+            model = model,
             api_key=GEMINI_API_KEY
         )
         
         self.rag_controller = RagController()
+        self.evaluator_llm = LangchainLLMWrapper(llm)
         
     def func_testing(self):
         rag_controller = RagController()
@@ -32,6 +41,10 @@ class RagEvaluation:
         
         agent = rag_controller.build_legal_agent(jurisdiction, domain)
         response = rag_controller.ask(agent, question)
+    
+    def run_evaluation(self, sample: list):
+        result = evaluate(dataset=sample,metrics=[LLMContextRecall(), Faithfulness(), FactualCorrectness()],llm=self.evaluator_llm)
+        print(result)
         
 if __name__== "__main__":
     try:
